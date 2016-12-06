9a9d0d8afd22035918a68f9bb832bce1ab557e79
brian lewis <brianburkelewis@gmail.com>
2016-12-06 11:16:12 -0800

    heroku updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, url_for
 from flask_bootstrap import Bootstrap
 import pip
+import os
 
 GL_KEY   = os.environ['GL_KEY']
 GL_EMAIL = os.environ['GL_EMAIL']
